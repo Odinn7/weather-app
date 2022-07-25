244a4d12adf996a9d874285ffb2c83bc513a8f1f
Andrew <odines.a7@gmail.com>
2022-07-25 16:44:48 +0400

    last

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,17 +1,13 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import citiesSlice from "./CitiesSlice";
 
-
-
 const rootReducer = combineReducers({
 	citiesSlice
 })
 
-
 export const store = configureStore({
 	reducer: rootReducer
 })
-
 
 export type RootState = ReturnType<typeof rootReducer>
 export type AppStore = typeof store
